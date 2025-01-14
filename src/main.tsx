f3d1a452170b0c71f69f4b691abbd04cf4f70d86
Rishik <rishik3555@gmail.com>
2025-01-14 16:39:21 +0530

    root update

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { Room } from './pages/Room';
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/room" element={<Room />} />
-        <Route path="/app" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Room />} />
+      <Route path="/room" element={<Room />} />
+      <Route path="/app" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
